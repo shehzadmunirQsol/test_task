986fa949b3b7465cd24e7fed2921033bde6a1183
hasanaliqureshi <hasanali.zeds@gmail.com>
2024-03-15 16:12:51 +0500

    init

---
+++ index.js
@@ -0,0 +1,91 @@
+const products = [
+    {
+        name: "T-shirt",
+        variants: [
+            {
+                title: "red",
+                price: 10
+            },
+            {
+                title: "green",
+                price: 10
+            }, {
+                title: "blue",
+                price: 10
+            }
+        ],
+        stock: 20,
+    },
+    {
+        name: "Jeans",
+        variants: [
+            {
+                title: "s",
+                price: 50
+            },
+            {
+                title: "m",
+                price: 50
+            }, {
+                title: "l",
+                price: 60
+            }
+        ],
+        stock: 3,
+    },
+    {
+        name: "Cap",
+        variants: [
+            {
+                title: "red",
+                price: 22
+            },
+            {
+                title: "green",
+                price: 22
+            }, {
+                title: "blue",
+                price: 22
+            }
+        ],
+        stock: 15,
+    }
+]
+
+
+// 1. Total Price of all products
+// Output: 256
+const task1 = () => {
+    
+}
+
+// 2. Stock information only
+// Output:
+// [
+//     { name: "T-shirt", stock: 20 }, { name: "Jeans", stock: 3 }, { name: "Cap", stock: 15 }
+// ]
+
+const task2 = () => {
+    
+}
+
+
+// 3. All products
+// Output:
+// ["T-shirt", "Jeans", "Cap"]
+
+const task3 = () => {
+    
+}
+
+// 4. T shirt green price
+// Output: 10
+
+const task4 = () => {
+    
+}
+
+console.log(`TASK 1  ===> `, task1())
+console.log(`TASK 2  ===> `, task2())
+console.log(`TASK 3  ===> `, task3())
+console.log(`TASK 4  ===> `, task4())
